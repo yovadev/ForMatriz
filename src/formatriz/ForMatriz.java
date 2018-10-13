e60ffdfc6549405e1f37c36d9b40493d80e92eb4
Yova <10067268+yovadev@users.noreply.github.com>
2018-10-13 10:39:00 -0600

    Initial commit

---
+++ src/formatriz/ForMatriz.java
@@ -0,0 +1,15 @@
+package formatriz;
+
+public class ForMatriz {
+
+    public static void main(String[] args) {
+       int numero[] = {1,2,3,4,5};
+       int suma = 0;
+       
+       for(int x: numero){
+           suma +=x;
+       }
+        System.out.println(suma);
+    }
+    
+}
